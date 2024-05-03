cc61ee4198138b0ca73e3f352ef9541522b91e73
Renan Tescaro <renantescaro@yahoo.com.br>
2024-05-02 21:45:50 -0300

    feat: add product image in checkout and style

--- src/pages/restaurant-list/components/CheckoutModal.tsx
+++ src/pages/restaurant-list/components/CheckoutModal.tsx
@@ -2,7 +2,10 @@ import * as React from 'react'
 import Modal from '@mui/material/Modal'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-
+import ListSubheader from '@mui/material/ListSubheader';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -34,23 +37,44 @@ const CheckoutModal = ({
             aria-describedby="modal-modal-description"
         >
             <Box sx={{ ...style, width: 400 }}>
-                <h2 id="child-modal-title">Carrinho</h2>
-                <p id="child-modal-description">
-                   
+                <p id="child-modal-title" style={{textAlign:'center', fontSize:25}}>
+                    Carrinho
                 </p>
-
-                <div>
+                <List
+                    sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+                    component="nav"
+                    aria-labelledby="nested-list-subheader"
+                    subheader={
+                        <ListSubheader component="div" id="nested-list-subheader">
+                            Itens adicionados
+                        </ListSubheader>
+                    }
+                >
                     {checkoutProducts.map((product, index) => (
-                        <div key={index}>
-                            <p>{product.item.name}</p>
-                            <p>Quantidade {product.quantity}</p>
-                            <p>R$ {product.item.price}</p>
+                    <ListItemButton>
+                        <img
+                            src={product.item.image}
+                            alt="product"
+                            style={{ width: 50, height: 50 , marginRight: 10}}
+                        />
+                        <div>
+                            <ListItemText primary={product.item.name} />
+                            <ListItemText primary={'R$ ' + product.item.price} />
                         </div>
+                    </ListItemButton>
                     ))}
+                </List>
+                <div style={{marginTop: 20}}>
+                    <Button onClick={onClose}>
+                        Adicionar mais itens
+                    </Button>
                 </div>
 
+                {/* Peça também */}
+                {/* cupom */}
+                
 
-                <Button onClick={onClose}>
+                <Button onClick={onClose} variant="contained">
                     Confirmar
                 </Button>
             </Box>
--- src/pages/restaurant-list/index.page.tsx
+++ src/pages/restaurant-list/index.page.tsx
@@ -20,25 +20,25 @@ export default function NestedList() {
         {
             name: 'Restaurante de Vó',
             products: [
-                { name: 'marmita pequena', price: 15.0 },
-                { name: 'marmita média', price: 20.0 },
-                { name: 'marmita grande', price: 25.0 }
+                { name: 'marmita pequena', price: 15.0, image:"https://via.placeholder.com/150" },
+                { name: 'marmita média', price: 20.0, image:"https://via.placeholder.com/150" },
+                { name: 'marmita grande', price: 25.0, image:"https://via.placeholder.com/150" }
             ]
         },
         {
             name: 'Café Bistro',
             products: [
-                { name: 'café Gelado', price: 10.0 },
-                { name: 'pão de queijo', price: 5.0 },
-                { name: 'suco de laranja', price: 10.0 }
+                { name: 'café Gelado', price: 10.0, image:"https://via.placeholder.com/150" },
+                { name: 'pão de queijo', price: 5.0, image:"https://via.placeholder.com/150" },
+                { name: 'suco de laranja', price: 10.0, image:"https://via.placeholder.com/150" }
             ]
         },
         {
             name: 'Só Filé',
             products: [
-                { name: 'filé de frango', price: 10.0 },
-                { name: 'file de tilapia', price: 10.0 },
-                { name: 'file de magnolia', price: 10.0 }
+                { name: 'filé de frango', price: 10.0, image:"https://via.placeholder.com/150" },
+                { name: 'file de tilapia', price: 10.0, image:"https://via.placeholder.com/150" },
+                { name: 'file de magnolia', price: 10.0, image:"https://via.placeholder.com/150" }
             ]
         }
     ]
